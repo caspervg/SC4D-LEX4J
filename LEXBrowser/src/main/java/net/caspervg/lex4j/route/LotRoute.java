e968de5d123451580df1ebbac81a3a1571561317
caspervg <tah9wae8>
2013-11-27 14:11:02 +0100

    removed useless import.. wtf intellij

--- LEXBrowser/src/main/java/net/caspervg/lex4j/route/LotRoute.java
+++ LEXBrowser/src/main/java/net/caspervg/lex4j/route/LotRoute.java
@@ -2,7 +2,6 @@ package net.caspervg.lex4j.route;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import net.caspervg.lex4j.auth.Auth;
 import net.caspervg.lex4j.bean.Comment;
