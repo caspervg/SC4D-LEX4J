3a317faabcde0e3bbfbd9515a3c4ca3063be1424
Casper Van Gheluwe <caspervg@gmail.com>
2014-08-05 08:43:36 +0200

    Add documentation for each individual filter

--- LEXBrowser/src/main/java/net/caspervg/lex4j/route/Filter.java
+++ LEXBrowser/src/main/java/net/caspervg/lex4j/route/Filter.java
@@ -6,18 +6,57 @@ package net.caspervg.lex4j.route;
  * @author Casper Van Gheluwe
  */
 public enum Filter {
+    /**
+     * Result to start at
+     */
     START("start"),
+    /**
+     * Number of results to return
+     */
     AMOUNT("amount"),
+    /**
+     * Ordering of the results
+     */
     ORDER_BY("order_by"),
+    /**
+     * Ascending or descending
+     */
     ORDER("order"),
+    /**
+     * Only return ID and name, no other information
+     */
     CONCISE("concise"),
+    /**
+     * Filter by author of the file
+     */
     CREATOR("creator"),
+    /**
+     * Filter by broad category of the file
+     */
     BROAD_CATEGORY("broad_category"),
+    /**
+     * Filter by LEX category of the file
+     */
     LEX_CATEGORY("lex_category"),
+    /**
+     * Filter by LEX type of the file
+     */
     LEX_TYPE("lex_type"),
+    /**
+     * Filter by broad type of the file (a very rough approach)
+     */
     BROAD_TYPE("broad_type"),
+    /**
+     * Filter by lot group of the file
+     */
     GROUP("group"),
+    /**
+     * Exclude results that are not LEX certified
+     */
     EXCLUDE_NOT_CERTIFIED("exclude_notcert"),
+    /**
+     * Exclude results that are locked (not downloadable)
+     */
     EXCLUDE_LOCKED("exclude_locked");
 
     private String representation;
